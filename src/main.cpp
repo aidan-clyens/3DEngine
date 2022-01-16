2f7778d6d175b918e34993c9ff94e54185ab5b13
Aidan Clyens <aidanc@rogers.com>
2022-01-15 22:25:39 -0500

    Remove glfw library and create window

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,35 @@
+// Includes
+#include <GLFW/glfw3.h>
+
 #include <iostream>
+
+// Defines
+#define SCREEN_WIDTH        1600
+#define SCREEN_HEIGHT       900
 
 
 int main(int argc, char **argv) {
-    std::cout << "Hello World!" << std::endl;
+    if (!glfwInit()) {
+        std::cerr << "Failed to init GLFW" << std::endl;        
+    }
+
+    GLFWwindow *window = glfwCreateWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Hello World", NULL, NULL);
+    if (!window) {
+        std::cerr << "Failed to create window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+
+    while (!glfwWindowShouldClose(window)) {
+        glClear(GL_COLOR_BUFFER_BIT);
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
 
     return 0;
 }
