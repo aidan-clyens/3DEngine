a6fd1387885361dbec57cea99940b67af59d7b76
Aidan Clyens <aidanc@rogers.com>
2022-01-15 23:09:24 -0500

    Draw a triangle on screen

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 // Includes
+#include <GL/glew.h>
 #include <GLFW/glfw3.h>
 
 #include <iostream>
@@ -6,6 +7,13 @@
 // Defines
 #define SCREEN_WIDTH        1600
 #define SCREEN_HEIGHT       900
+
+
+float vertices[] = {
+    0.0, 0.5, 0.0,       // Vertex 1
+    0.5, -0.5, 0.0,      // Vertex 2
+    -0.5, -0.5, 0.0      // Vertex 3
+};
 
 
 int main(int argc, char **argv) {
@@ -21,9 +29,19 @@ int main(int argc, char **argv) {
     }
 
     glfwMakeContextCurrent(window);
+    glfwSwapInterval(1);
 
     while (!glfwWindowShouldClose(window)) {
+        // Clear window
         glClear(GL_COLOR_BUFFER_BIT);
+
+        // Draw vertex array
+        glEnableClientState(GL_VERTEX_ARRAY);
+        glVertexPointer(3, GL_FLOAT, 0, vertices);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glDisableClientState(GL_VERTEX_ARRAY);
+
+        // Swap buffer
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
