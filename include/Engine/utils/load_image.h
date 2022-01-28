930dd7cb637036512a038e872c375120621ebc7d
Aidan <aidanc@rogers.com>
2022-01-28 09:20:20 -0500

    Add stb_image_write.h

--- include/Engine/utils/load_image.h
+++ include/Engine/utils/load_image.h
@@ -6,3 +6,5 @@
 unsigned char *load_image_data(const std::string &path, int *width, int *height, int *num_channels);
 
 void free_image_data(unsigned char *data);
+
+int write_png(const std::string &filename, int w, int h, int comp, const void *data, int stride_in_bytes);
--- src/Engine/utils/load_image.cpp
+++ src/Engine/utils/load_image.cpp
@@ -5,6 +5,11 @@
 #endif
 #include "stb_image.h"
 
+#ifndef STB_IMAGE_WRITE_IMPLEMENTATION
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#endif
+#include "stb_image_write.h"
+
 
 unsigned char *load_image_data(const std::string &path, int *width, int *height, int *num_channels) {
     return stbi_load(path.c_str(), width, height, num_channels, 0);
@@ -12,4 +17,9 @@ unsigned char *load_image_data(const std::string &path, int *width, int *height,
 
 void free_image_data(unsigned char *data) {
     stbi_image_free(data);
+}
+
+
+int write_png(const std::string &filename, int w, int h, int comp, const void *data, int stride_in_bytes) {
+    return stbi_write_png(filename.c_str(), w, h, comp, data, stride_in_bytes);
 }
